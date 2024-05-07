319e5bc8720a655557b0d3db0d973306ade0d82e
vicziani <viczian.istvan@gmail.com>
2024-05-07 14:59:11 +0200

    POST

--- hello-spring/src/main/java/hello/employee/EmployeeController.java
+++ hello-spring/src/main/java/hello/employee/EmployeeController.java
@@ -1,8 +1,12 @@
 package hello.employee;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,8 +17,13 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @PostMapping
-    public void save(@RequestBody Employee employee) {
+    public ResponseEntity<Employee> save(@RequestBody Employee employee,
+                                         UriComponentsBuilder builder) {
         employeeService.save(employee);
+
+        return ResponseEntity
+                .created(builder.path("/api/employees/{id}").buildAndExpand(employee.getId()).toUri())
+                .body(employee);
     }
 
     @GetMapping
