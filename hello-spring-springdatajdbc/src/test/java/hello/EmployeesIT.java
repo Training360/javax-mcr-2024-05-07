e54afa5b737b1b61350eea3de88e11982d0af2ad
vicziani <viczian.istvan@gmail.com>
2024-05-08 14:47:07 +0200

    Integrációs tesztek

---
+++ hello-spring-springdatajdbc/src/test/java/hello/EmployeesIT.java
@@ -0,0 +1,41 @@
+package hello;
+
+import hello.employee.EmployeeResourceDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(statements = "delete from employees")
+class EmployeesIT {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void create() {
+        webTestClient
+                .post()
+                .uri("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                            "name": "John Doe"
+                        }
+                        """)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody().jsonPath("name").isEqualTo("John Doe");
+
+        webTestClient
+                .get()
+                .uri("/api/employees")
+                .exchange()
+                .expectBodyList(EmployeeResourceDto.class).hasSize(1);
+
+    }
+}
