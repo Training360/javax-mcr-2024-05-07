a011154088cbe3537d5deb49090d62ff6f9d2000
vicziani <viczian.istvan@gmail.com>
2024-05-09 09:17:54 +0200

    bigserial deprecated

--- hello-spring-springdatajdbc/src/main/resources/db/changelog/01-create-employees.sql
+++ hello-spring-springdatajdbc/src/main/resources/db/changelog/01-create-employees.sql
@@ -1,3 +1,3 @@
-create table employees (id bigserial not null, emp_name varchar(255), primary key (id));
+create table employees (id bigint generated always as identity, emp_name varchar(255), primary key (id));
 insert into employees(emp_name) values ('John Doe');
 insert into employees(emp_name) values ('Jack Doe');
